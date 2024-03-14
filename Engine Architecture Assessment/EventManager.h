10aaf3911bd3be67200296116c8fa2854b88e1d1
MattB166 <matthewbrake2001@hotmail.co.uk>
2024-03-14 15:26:37 +0000

    couple of commented additions

--- Engine Architecture Assessment/EventManager.h
+++ Engine Architecture Assessment/EventManager.h
@@ -8,13 +8,15 @@ struct CollisionEvent
 class EventManager
 {
 public: 
-	static EventManager* instance() { return _instance != nullptr ? _instance : _instance = new EventManager(); }
+	static EventManager* instance() { return _instance != nullptr ? _instance : _instance = new EventManager(); } ///might not need to be an instance
+   //as each object might need its own event manager 
 
 private:
 	static EventManager* _instance;
 	///hold a queue of events 
 	/// maybe something like? 
 	std::queue<CollisionEvent> EventQueue;
+	///also need a list of registered listeners 
 	void DispatchEvents();
 	///might need reference to listeners here too. struct of some sort 
 };
